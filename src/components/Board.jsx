dcca87e789915057d93f279187750e3387952fbe
GowtherZa <GowtherZa@proton.me>
2024-03-09 14:49:48 -0500

    feat: Tiles centradas

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,7 +3,7 @@ import TileRow from "./TileRow";
 export default function Board() {
   return (
     <div className="w-screen h-screen bg-black bg-opacity-25 flex justify-center items-center">
-      <div className="bg-slate-500 w-2/4 h-3/4 rounded-lg shadow">
+      <div className="bg-slate-500 w-2/4 h-3/4 rounded-lg shadow flex justify-center items-center">
         <div className="flex flex-wrap flex-col content-center items-center">
           <TileRow></TileRow>
           <TileRow></TileRow>
--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,5 +1,5 @@
 export default function Tile() {
   return (
-    <div className="border-2 border-slate-200 rounded-lg m-2 w-16 h-16 "></div>
+    <div className="border-2 border-slate-200 rounded-full m-2 w-16 h-16 "></div>
   );
 }
